1002aaaf8b95fd2126f8ff4550e6830bfdbd7ce9
Muhammed Tastan <muha082195@gmail.com>
2025-04-02 22:41:09 +0200

    Tilføjet 2 nye skærme, fjernet sækunder konstruktor fra Birthday.kt som ikke bliver  brugt

--- app/src/main/java/com/example/mybirthdayapp/model/Birthday.kt
+++ app/src/main/java/com/example/mybirthdayapp/model/Birthday.kt
@@ -2,7 +2,7 @@ package com.example.mybirthdayapp.model
 
 data class Birthday(
     val id: Int,
-    val userId:String,
+    val userId: String,
     val name: String,
     val birthYear: Int,
     val birthMonth: Int,
@@ -10,19 +10,7 @@ data class Birthday(
     val remarks: String?,
     val pictureUrl: String?,
     val age: Int
-
-){
-    constructor(
-        userId: String,
-        name: String,
-        birthYear: Int,
-        birthMonth: Int,
-        birthDayOfMonth: Int,
-        remarks: String,
-        pictureUrl: String,
-        age: Int
-    ) : this(-1, userId, name, birthYear, birthMonth, birthDayOfMonth, remarks, pictureUrl, age)
-
+) {
     override fun toString(): String {
         return "$id Owner: $userId, Name: $name, Birth Year: $birthYear, Birth Month: $birthMonth, Birth Day: $birthDayOfMonth, Remarks: $remarks, PictureUrl: $pictureUrl, Age: $age"
     }
